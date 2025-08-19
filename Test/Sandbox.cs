7d9894c4a7cbadd77273cfa05ab3b02bd9d39b97
JFomit <tzaberejny@gmail.com>
2025-08-19 19:24:19 +0300

    test: linq

--- Test/Sandbox.cs
+++ Test/Sandbox.cs
@@ -1,5 +1,10 @@
+using BenchmarkDotNet.Attributes;
 using JFomit.Functional.Extensions;
+using JFomit.Functional.Monads;
 using static JFomit.Functional.Prelude;
+
+var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Bench>();
+return;
 
 var num = Some(32);
 var name = Some("hello");
@@ -7,3 +12,44 @@ var name = Some("hello");
 var r = from n in num
         from a in name
         select a + n;
+
+[MemoryDiagnoser]
+public class Bench
+{
+    [Benchmark]
+    public Option<Data> TestAllocations()
+    {
+        var b = new Builder();
+        return b.WithX(42).WithY("ss").WithZ(3.14f).Build();
+    }
+}
+
+public record Data(int X, string Y, float Z);
+public class Builder
+{
+    Option<int> X = None;
+    Option<string> Y = None;
+    Option<float> Z = None;
+
+    public Builder WithX(int x)
+    {
+        X = Some(x);
+        return this;
+    }
+    public Builder WithY(string y)
+    {
+        Y = Some(y);
+        return this;
+    }
+    public Builder WithZ(float z)
+    {
+        Z = Some(z);
+        return this;
+    }
+
+    public Option<Data> Build()
+        => from x in X
+           from y in Y
+           from z in Z
+           select new Data(x, y, z);
+}
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using JFomit.Functional.Extensions;
+using JFomit.Functional.Monads;
 using static JFomit.Functional.Prelude;
 
 namespace Test;
@@ -8,6 +9,14 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        
+        var num = Some(32);
+        var name = Some("hello");
+
+        var r = from n in num
+                where n > 42
+                from a in name
+                select a + n;
+
+        Assert.Equal(None, r);
     }
 }
